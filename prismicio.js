aa48d723de80275b147d883b6e4d07196641174b
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2022-04-28 14:17:18 +0200

    adding multi repo handling

--- prismicio.js
+++ prismicio.js
@@ -2,7 +2,7 @@ import * as prismic from '@prismicio/client'
 import { enableAutoPreviews } from '@prismicio/next'
 import sm from './sm.json'
 
-export const endpoint = sm.apiEndpoint
+export const endpoint = process.env.API_ENDPOINT ? process.env.API_ENDPOINT : sm.apiEndpoint
 export const repositoryName = prismic.getRepositoryName(endpoint)
 
 //Update the Link Resolver to match your project's route structure
