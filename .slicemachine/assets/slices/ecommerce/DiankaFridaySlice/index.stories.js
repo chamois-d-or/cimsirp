e63e3fb62397fde82cc5bc76fa95c9570c3d6b76
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2022-11-04 10:46:08 +0100

    diankafridayslice test

---
+++ .slicemachine/assets/slices/ecommerce/DiankaFridaySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../../slices/ecommerce/DiankaFridaySlice';
+
+export default {
+  title: 'slices/ecommerce/DiankaFridaySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Fresh","spans":[]}],"description":[{"type":"paragraph","text":"Officia esse sit duis eiusmod ex laborum excepteur laborum. Reprehenderit irure velit sunt sint pariatur nostrud Lorem excepteur reprehenderit irure aute incididunt velit sint officia. Nisi ullamco exercitation irure nostrud ipsum ut tempor quis et do nulla ut ea anim ipsum.","spans":[]}]},"slice_type":"dianka_friday_slice","id":"_Default"}} />
+_Default.storyName = ''
