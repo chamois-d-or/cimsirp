b81bc5d14cb43c299dbf7a398a0cd8a530016a7c
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2022-11-29 17:50:39 +0100

    remove bj

--- .slicemachine/assets/slices/ecommerce/BenjaminSlice/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../../slices/ecommerce/BenjaminSlice';
-
-export default {
-  title: 'slices/ecommerce/BenjaminSlice'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Difficulty","spans":[]}],"description":[{"type":"paragraph","text":"Mollit quis tempor nostrud consectetur et fugiat reprehenderit exercitation sunt consequat nisi mollit.","spans":[]}]},"slice_type":"benjamin_slice","id":"_Default"}} />
-_Default.storyName = ''
